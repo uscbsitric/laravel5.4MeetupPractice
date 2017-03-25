496cbf5a320342f17d4a76f8d481a84e2776cac6
Frederick Sandalo <uscbsitric@gmail.com>
2017-03-26 05:50:13 +0800

    Events Step 5: Defining an Event

--- app/Events/TheBertrandSteppedIn.php
+++ app/Events/TheBertrandSteppedIn.php
@@ -10,18 +10,20 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
+// As you can see, this event class contains no logic. It is simply a container for the Bertrand "User" instance that just stepped in.
 class TheBertrandSteppedIn
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels; // The SerializesModels trait used by the event will gracefully serialize any Eloquent models if the event object is serialized using PHP's serialize function.
 
+	public $user;
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
