da1f6fca834330c1d77617bfdef6575f493ee232
Frederick Sandalo <uscbsitric@gmail.com>
2017-03-26 06:42:08 +0800

    Events Step 6: Defining Listeners

--- app/Listeners/MeetupCrowd.php
+++ app/Listeners/MeetupCrowd.php
@@ -5,6 +5,8 @@ namespace App\Listeners;
 use App\Events\TheBertrandSteppedIn;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class MeetupCrowd
 {
@@ -26,6 +28,11 @@ class MeetupCrowd
      */
     public function handle(TheBertrandSteppedIn $event)
     {
-        //
+        $user = $event->user;
+		$id = $user->id;
+		$now = Carbon::now();
+		sleep(10);
+		$logMessage = "Non Queued event: User with ID: $id flocked by the Meetup Crowd except me.  And they were finished with him at " . $now;
+		Log::info($logMessage);
     }
 }
--- app/Listeners/SecurityGuard.php
+++ app/Listeners/SecurityGuard.php
@@ -5,7 +5,12 @@ namespace App\Listeners;
 use App\Events\TheBertrandSteppedIn;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
+// Event listeners receive the event instance in their handle method. 
+// The event:generate command will automatically import the proper event class and type-hint the event on the handle method. 
+// Within the handle method, you may perform any actions necessary to respond to the event:
 class SecurityGuard
 {
     /**
@@ -26,6 +31,11 @@ class SecurityGuard
      */
     public function handle(TheBertrandSteppedIn $event)
     {
-        //
+        $user = $event->user;
+		$id = $user->id;
+		$now = Carbon::now();
+		sleep(10);
+		$logMessage = "Non Queued event: User with ID: $id gave his autograph to the guard and finished at " . $now;
+		Log::info($logMessage);
     }
 }
