10da1a35f71d3ef74722a57493085bedfac1b7dd
Frederick Sandalo <uscbsitric@gmail.com>
2017-03-29 18:10:12 +0800

    Events Step 8.4: Configure the Listener

--- app/Listeners/PresentationProcessor.php
+++ app/Listeners/PresentationProcessor.php
@@ -26,6 +26,10 @@ class PresentationProcessor
      */
     public function handle(LaravelMeetupPresentation $event)
     {
-        //
+		sleep(20);
+        $user = $event->user;
+		$id = $user->id;
+		$logMessage = "Queued event: User with ID: $id for testing purposes and this is a 20 second process.";
+		Log::info($logMessage);
     }
 }
