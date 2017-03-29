ac9bfeebae9376188d6baf3f9177e6d785531cdc
Frederick Sandalo <uscbsitric@gmail.com>
2017-03-29 17:58:15 +0800

    Events Step 8.3: Configure the Event

--- app/Events/LaravelMeetupPresentation.php
+++ app/Events/LaravelMeetupPresentation.php
@@ -10,25 +10,18 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
+use App\User;
+
 class LaravelMeetupPresentation
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $user;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+      $this->user = $user;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return Channel|array
-     */
     public function broadcastOn()
     {
         return new PrivateChannel('channel-name');
