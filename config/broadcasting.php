a2198a39b199dd3675fb3abf482be9e47111b5a4
Frederick Sandalo <uscbsitric@gmail.com>
2017-03-30 11:53:29 +0800

    Broadcasting Step 1: Broadcasting Configuration

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,9 +35,9 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                //
-            ],
+            'options' =>  ['cluster'   => env('PUSHER_API_CLUSTER'),
+                     	   'encrypted' => true // since its a boolean value, I did not place this in .env
+                          ],
         ],
 
         'redis' => [
