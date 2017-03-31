63e2f783e79abd82b920edb9522bf2a900facdcb
Frederick Sandalo <uscbsitric@gmail.com>
2017-03-31 16:11:57 +0800

    Broadcasting Step 4: Create a route for an Event for Broadcast to Pusher

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::get('/queueTesting',
 Route::get('/queuedEvent',
            'EventsTestingController@queuedEvent'
 		  );
+Route::get('/publicEventForBroadcast',
+           'EventsTestingController@publicEventForBroadcast'
+		  );
